2ed1a8dc302669725e4c8b4bcf81154dfa375548
lirui <1554500061@qq.com>
2017-11-28 21:50:55 +0800

    修正部分不能拼接在一起的句子；限定只抽取大写字母的情态词句子

--- process_content.py
+++ process_content.py
@@ -4,27 +4,48 @@ function: 过滤带有情态动词的句子
 '''
 from global_config import *
 
+
 def get_sentences(rfc_file):
     sentences = []
     # 获取content
-    pattern = '^(\s+).*'  # 行首的空白符
+    pattern_space = '^(\s+).*'  # 行首的空白符
+    pattern_unword = '(^(\s+)[^a-zA-Z]*)'  # 行首的非字母字符
     content = ''
     last_space = ''
     cur_space = ''
+
+    last_unword_len = 0
+    cur_unword_len = 0
+
     for line in open(rfc_file,'r'):
-        match_results = re.compile(pattern).findall(line)
-        if len(match_results):
-            cur_space = match_results[0]
+        line = line.strip()
+        if line.strip()=='':
+            content += '\n'
+            continue
+
+        space_results = re.compile(pattern_space).findall(line)
+        if len(space_results):
+            cur_space = space_results[0]
         else:
             cur_space = ''
 
-        line = line.strip()
-        if cur_space!=last_space:
-            content += '\n'+line
+        unword_results = re.compile(pattern_unword).findall(line)
+        if len(unword_results) and len(unword_results[0]):
+            cur_unword_len = len(unword_results[0])
+        else:
+            cur_unword_len = 0
+
+        if cur_space != last_space:
+            # 比较unword长度
+            if cur_unword_len == last_unword_len:
+                content += ' ' + line
+            else:
+                content += '\n'+line
         else:
             content += ' ' + line
 
         last_space = cur_space
+        last_unword_len = cur_unword_len
 
     # 去除页眉页脚
     pattern = '\s+Fielding, et al.*\s+.*\s+'
@@ -40,8 +61,6 @@ def get_sentences(rfc_file):
     return sentences
 
 
-
-
 def match_modal(outfile,rfc_file):
     sentences = get_sentences(rfc_file)
     fout = open(outfile, 'w')
@@ -49,20 +68,33 @@ def match_modal(outfile,rfc_file):
     for sent in sentences:
         tags = ''
         flag = False
+        # for pattern in modal_pattern_list:
         for pattern in modal_pattern_list:
+            pattern = pattern.upper()
             results = re.findall(pattern, sent)
             if results:
                 flag = True
-                tags += '['+ pattern+']'
+                tags += '['+ pattern+']\t'
         if flag:
             fout.write(tags+'\n'+sent+'\n\n\n\n')
     fout.close()
 
 
 
-# match_modal(contain_modal_file_2616,rfc2616_file)
+# def match_modal(outfile,rfc_file):
+#     sentences = get_sentences(rfc_file)
+#     fout = open(outfile, 'w')
+#
+#     for sent in sentences:
+#         fout.write(sent + '\n\n\n\n')
+#     fout.close()
+
+
+
+match_modal(contain_modal_file_2616,rfc2616_file)
 match_modal(contain_modal_file_7230,rfc7230_file)
 
 
+# match_modal('tmp_chenhe.txt','./corpus/tmp.txt')
 
-# 词性过滤
+
